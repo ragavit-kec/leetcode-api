949c55f65cfd9932370c19c8d8d82acdfe9cac24
ragavi <127740526+ragavit-kec@users.noreply.github.com>
2025-06-25 09:30:45 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 import config from './config';
 
-app.listen(config.port, () => {
+app.listen(config.port, '0.0.0.0', () => {
   console.log(`Server is running at => http://localhost:${config.port} ⚙️`);
 });
